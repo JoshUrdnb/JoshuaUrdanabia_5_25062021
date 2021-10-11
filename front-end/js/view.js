026b82776df7c335f5805266071f910b07875006
JoshUrdnb <76445762+JoshUrdnb@users.noreply.github.com>
2021-10-11 22:37:03 +0200

    show elements on homepage

--- front-end/js/view.js
+++ front-end/js/view.js
@@ -1,3 +1,9 @@
 const afficherCartesCamera = (camera) => {
     console.log('view', camera);
+
+    document.getElementById("main").innerHTML += `<img class="test" id="img" src="${camera.imageUrl}"/>`
+    document.getElementById("main").innerHTML += `<div class="texter" id="text">${camera.name}</div>`
+    document.getElementById("main").innerHTML += `<div id="text">${camera.description}</div>`
+    document.getElementById("main").innerHTML += `<div id="text">${camera.price}</div>`
 }
+
