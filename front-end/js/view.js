86c80dca0b6318f92e41e51ef9de34b369ba1355
JoshUrdnb <76445762+JoshUrdnb@users.noreply.github.com>
2021-10-13 03:51:01 +0200

    amelioration

--- front-end/js/view.js
+++ front-end/js/view.js
@@ -2,12 +2,13 @@ const afficherCartesCamera = (camera) => {
     console.log('view', camera);
 
     document.getElementById('main').innerHTML += `
-    <article class="camArticle">
-        <img class="camImg" src="${camera.imageUrl}">
-        <h3 class="camName">${camera.name}</h3>
-        <p class="camDescription">${camera.description}</p>
-        <p class="camPrice">${camera.price}</p>
-    </article>
+    <a href="html/products.html">
+        <article class="camArticle">
+            <img class="camImg" src="${camera.imageUrl}">
+            <h3 class="camName">${camera.name}</h3>
+            <p class="camDescription">${camera.description}</p>
+            <p class="camPrice">${camera.price}</p>
+        </article>
+    </a>
     `
 }
-
