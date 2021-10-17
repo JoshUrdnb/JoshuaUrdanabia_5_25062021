2f8d2314102756b6467268c3d6d4739e35e2791f
JoshUrdnb <76445762+JoshUrdnb@users.noreply.github.com>
2021-10-17 18:22:45 +0200

    Modification html & links

--- front-end/js/view.js
+++ front-end/js/view.js
@@ -4,10 +4,12 @@ const afficherCartesCamera = (camera) => {
     document.getElementById('main').innerHTML += `
         <article class="camArticle">
             <img class="camImg" src="${camera.imageUrl}">
-            <h3 class="camName">${camera.name}</h3>
-            <p class="camDescription">${camera.description}</p>
-            <p class="camPrice">${camera.price}</p>
-            <a href="html/products.html" class="productBtn">Plus d'infos</a>
+                <div class="cardContent">
+                    <h3 class="camName">${camera.name}</h3>
+                    <p class="camDescription">${camera.description}</p>
+                    <p class="camPrice">${camera.price}</p>
+                    <a href="html/products.html" class="productBtn">Plus d'infos</a>
+                </div>
         </article>
     `
 }
