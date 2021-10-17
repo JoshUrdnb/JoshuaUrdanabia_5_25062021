e3f9106b4035f987410cdd31d2acba7d199b15d5
JoshUrdnb <76445762+JoshUrdnb@users.noreply.github.com>
2021-10-17 17:33:22 +0200

    Homepage Overhaul

--- front-end/js/view.js
+++ front-end/js/view.js
@@ -2,13 +2,12 @@ const afficherCartesCamera = (camera) => {
     console.log('view', camera);
 
     document.getElementById('main').innerHTML += `
-    <a href="html/products.html">
         <article class="camArticle">
             <img class="camImg" src="${camera.imageUrl}">
             <h3 class="camName">${camera.name}</h3>
             <p class="camDescription">${camera.description}</p>
             <p class="camPrice">${camera.price}</p>
+            <a href="html/products.html" class="productBtn">Plus d'infos</a>
         </article>
-    </a>
     `
 }
