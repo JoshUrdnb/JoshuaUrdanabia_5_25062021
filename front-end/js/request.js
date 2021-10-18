f3b0ea905e32a78d932371e957fee204f79f55a2
JoshUrdnb <76445762+JoshUrdnb@users.noreply.github.com>
2021-10-19 01:09:29 +0200

    try+catch & alert added

--- front-end/js/request.js
+++ front-end/js/request.js
@@ -1,4 +1,8 @@
 const dataApi = async (url) => {
-    const response = await fetch(url);
-    return await response.json();
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (probleme) {
+        alert("Un problème est survenu, veuillez réessayer ultérieurement.");
+    }
 }
