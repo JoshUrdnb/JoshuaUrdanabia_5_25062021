6845f6f5cf5e4e7532931cf185fac3f909883c43
JoshUrdnb <76445762+JoshUrdnb@users.noreply.github.com>
2021-11-16 17:06:45 +0100

    query string error 404

--- front-end/js/products.js
+++ front-end/js/products.js
@@ -2,6 +2,26 @@ const products = () => {
     const queryString = window.location.search;
     const urlSearchParams = new URLSearchParams(queryString);
     const productId = urlSearchParams.get('id');
+    console.log(productId);
+
+    if (productId === null) {
+        document.getElementById("products").innerHTML = `
+        <article class="camDetails">
+        <p class="nullDetail">Le produit n'existe pas.</p>
+        </article> `}
+
+    if (productId.length > 24) {
+        document.getElementById("products").innerHTML = `
+        <article class="camDetails">
+        <p class="nullDetail">La page demandée n'existe pas.</p>
+        </article> `}
+
+    if (productId.length < 24) {
+        document.getElementById("products").innerHTML = `
+        <article class="camDetails">
+        <p class="nullDetail">La page demandée n'existe pas.</p>
+        </article> `}
+
     const getProduct = dataApi(`http://localhost:3000/api/cameras/${productId}`);
     //const getProduct2 = data('http://localhost:3000/api/cameras/' + productId);
     getProduct.then((product) => {
