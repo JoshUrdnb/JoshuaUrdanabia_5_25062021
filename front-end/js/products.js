9695be8fbfadc18f3bef24e672ffff9559b134b5
JoshUrdnb <76445762+JoshUrdnb@users.noreply.github.com>
2021-11-23 15:22:16 +0100

    true on product

--- front-end/js/products.js
+++ front-end/js/products.js
@@ -2,12 +2,12 @@ const products = () => {
     const queryString = window.location.search;
     const urlSearchParams = new URLSearchParams(queryString);
     const productId = urlSearchParams.get('id');
-    console.log(productId);
-
     const getProduct = dataApi(`http://localhost:3000/api/cameras/${productId}`);
     //const getProduct2 = data('http://localhost:3000/api/cameras/' + productId);
     getProduct.then((product) => {
-            afficherProduct(product);
+
+            afficherProduct(product) = true;
+            
             if (product === null) {
                 afficherErreur("La page demandée n'existe pas.");
             }
