b3e85358f127005efe7bb0686303748c0121191d
JoshUrdnb <76445762+JoshUrdnb@users.noreply.github.com>
2021-11-30 22:38:19 +0100

    Pour ajouter un produit dans le panier

--- front-end/js/products.js
+++ front-end/js/products.js
@@ -5,16 +5,7 @@ const products = () => {
     const getProduct = dataApi(`http://localhost:3000/api/cameras/${productId}`);
     //const getProduct2 = data('http://localhost:3000/api/cameras/' + productId);
     getProduct.then((product) => {
-        console.log(product);
-
-        localStorage.setItem('Zurss 50S', 'Zurss 50S');
-        localStorage.setItem('Hirsch 400DTS', 'Hirsch 400DTS');
-        localStorage.setItem('Franck JS 105', 'Franck JS 105');
-        localStorage.setItem('Kuros TTS', 'Kuros TTS');
-        localStorage.setItem('Katatone', 'Katatone');
-
-        console.log(localStorage.getItem('panier'));
-
+        //console.log(product);
         if (product !== null) {
             afficherProduct(product);
         } else if (product === null) {
