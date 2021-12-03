e4519fcfc710e8c4fec67ae753bf2389b91e39ef
JoshUrdnb <76445762+JoshUrdnb@users.noreply.github.com>
2021-12-03 23:40:30 +0100

    Afficher les lignes du panier

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,10 +1,10 @@
 const main = () => {
     const cameras = dataApi('http://localhost:3000/api/cameras');
     cameras.then((toutesCameras) => {
-        console.log(toutesCameras);
+        //console.log(toutesCameras);
         toutesCameras.forEach((camera) => {
             afficherCartesCamera(camera);
-            console.log(camera);
+            //console.log(camera);
         });
     })
 }
--- front-end/js/view.js
+++ front-end/js/view.js
@@ -1,6 +1,6 @@
 // Afficher les cartes des appareils photos :
 const afficherCartesCamera = (camera) => {
-    console.log('view', camera);
+    //console.log('view', camera);
 
     document.getElementById('main').innerHTML += `
         <article class="camArticle">
@@ -42,18 +42,56 @@ const afficherProduct = (camera) => {
 
             <div class="camPrice">${formatToCurrency(camera.price, 'EUR', 'fr-FR')}</div>
             
-            <a href="cart.html" class="orderBtn" id="ajouter">Ajouter au panier</a>
+            <a href="cart.html" class="orderBtn" type="submit" name="ajouter" id="ajouter">Ajouter au panier</a>
     </article> `;
     document.getElementById('ajouter').addEventListener('click', (event) => {
         event.preventDefault();
         //console.log(camera); 
         let panier = JSON.parse(localStorage.getItem('panier')) || [];
         panier.push(camera);
-        localStorage.setItem('panier',JSON.stringify(panier));
+        localStorage.setItem('panier', JSON.stringify(panier));
         console.log(panier);
-        
     })
 }
+
+// Recuperer les valeurs :
+const afficherLigneDuPanier = (produit) => {
+    console.log(produit);
+}
+
+/*const updatePanier = () => {
+    if(panier.lenght > 0) {
+        document.getElementById('panierId').innerHTML += `
+            <ul id="cart_list">
+                <li class="cart_item clearfix bgc-element">
+                    <div class="cart_item_image"><img src="${camera.imageUrl}" alt=""></div>
+                    <div
+                        class="cart_item_info d-flex flex-md-row flex-column justify-content-between">
+                        <div class="cart_item_name cart_info_col">
+                            <div class="cart_item_title">Nom</div>
+                            <div class="cart_item_text" id="productName">${camera.name}</div>
+                        </div>
+                        <div class="cart_item_color cart_info_col">
+                            <div class="cart_item_title">Lentille</div>
+                            <div class="cart_item_text">24-60mm 2.8/4.5</div>
+                        </div>
+                        <div class="cart_item_quantity cart_info_col">
+                            <div class="cart_item_title">Quantité</div>
+                            <div class="cart_item_text">1</div>
+                        </div>
+                        <div class="cart_item_price cart_info_col">
+                            <div class="cart_item_title">Prix</div>
+                            <div class="cart_item_text" id="productPrice">${formatToCurrency(camera.price, 'EUR', 'fr-FR')}</div>
+                        </div>
+                        <div class="cart_item_total cart_info_col">
+                            <div class="cart_item_title">Total</div>
+                            <div class="cart_item_text">3099,00€</div>
+                        </div>
+                    </div>
+                </li>
+            </ul> `;
+    };
+}*/
 
 // Afficher message d'erreur :
 const afficherErreur = (message) => {
